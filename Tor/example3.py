abf9d4d3a42b8a3e75cada0316cc78c07e512c1c
Martin <martinstojanovic96@gmail.com>
2023-04-29 15:23:03 +0200

    add tor script

---
+++ Tor/example3.py
@@ -0,0 +1,11 @@
+from torpy.http.requests import TorRequests
+
+with TorRequests() as tor_requests:
+    print("build circuit")
+    with tor_requests.get_session() as sess:
+        print(sess.get("http://httpbin.org/ip").json())
+        print(sess.get("http://httpbin.org/ip").json())
+        print("renew circuit")
+        with tor_requests.get_session() as sess:
+            print(sess.get("http://httpbin.org/ip").json())
+            print(sess.get("http://httpbin.org/ip").json())
